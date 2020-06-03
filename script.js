810ace2dabd3da4f7edc6c4c19acf65fb004e150
Andressa <dressalsl97@gmail.com>
2020-06-03 14:01:17 -0300

    populate options ok

---
+++ script.js
@@ -0,0 +1,42 @@
+const countrySelect = document.querySelector("select[name=country]");
+// const countryName = document.querySelector("select[name=country-name]");
+
+
+
+countrySelect.addEventListener("change",populateCountry)
+// countryName.addEventListener('submit', getNameCountry);
+
+function populateCountry() {
+    const url = "http://coronavirus-19-api.herokuapp.com/countries";
+    fetch(url)
+    .then( (res) => res.json())
+    .then( countries => {
+        for (const country of countries) {
+            countrySelect.innerHTML += `<option value = "">${country.country}</option>`
+        }
+        
+    })
+    
+}
+
+populateCountry()
+
+// function getNameCountry(e){
+//     e.preventDefault();
+
+//     fetch(`http://coronavirus-19-api.herokuapp.com/countries/${country}`)
+// }
+
+// function getNameCountry(event) {
+//     const countrySelect = document.querySelector("[name=country]");
+//     const url = "http://coronavirus-19-api.herokuapp.com/countries/${country}";
+//     fetch(url)
+//     console.log(url)
+//     .then((response) => response.json())
+//     .then( countries =>{
+//         for (const country of countries) {
+//             countrySelect.innerHTML += `<h2>${country.country}</h2>`
+//         }
+//     }
+// }
+    
