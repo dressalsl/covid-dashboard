a498c2ce4203e135886fe9fe982e6487ed379dab
Andressa <dressalsl97@gmail.com>
2020-06-20 00:28:22 -0300

    api funcionando

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
-const countrySelect = document.querySelector("select[name=country]");
-const countryName = document.querySelector(".country-name");
+const countrySelect = document.querySelector("#country-select");
 
+const countryNameTitle = document.querySelector(".county-name-title")
+
+const countryDiaryCases = document.querySelector("#diary-cases");
+const countryMilionCases = document.querySelector("#milion-cases");
+const countryAllCases = document.querySelector("#all-cases");
+
+const countryDiaryDeaths = document.querySelector("#diary-deaths");
+const countryMilionDeaths = document.querySelector("#milion-deaths");
+const countryAllDeaths = document.querySelector("#all-deaths");
+
+const countryMilionTests = document.querySelector("#milion-tests");
+const countryAllTests = document.querySelector("#all-tests");
+
+const countryActiveCases = document.querySelector("#active-cases");
+const countryCriticalCases = document.querySelector("#critical-cases");
+const countryRecovered = document.querySelector("#recovered");
+
+
+let start = "World";
+
+
+const getCovidDashboard = async country => {
+    try {
+        const covidApi = await fetch(`http://coronavirus-19-api.herokuapp.com/countries/${country}`)
+ 
+        const data = await covidApi.json();
+        console.log(data);
+
+
+    countryNameTitle.innerHTML = `${data.country}`
+
+    countryDiaryCases.innerHTML = `${data.todayCases}`
+    countryMilionCases.innerHTML = `${data.casesPerOneMillion}`
+    countryAllCases.innerHTML = `${data.cases}`
+
+    countryDiaryDeaths.innerHTML = `${data.todayDeaths}`
+    countryMilionDeaths.innerHTML = `${data.deathsPerOneMillion}`
+    countryAllDeaths.innerHTML = `${data.deaths}`
+
+    countryMilionTests.innerHTML = `${data.testsPerOneMillion}`
+    countryAllTests.innerHTML = `${data.totalTests}`
+
+    countryActiveCases.innerHTML = `${data.active}`
+    countryCriticalCases.innerHTML = `${data.critical}`
+    countryRecovered.innerHTML = `${data.recovered}`
+    
+    }catch(err){
+        console.log(err);
+    }
+
+}
 
 function populateCountry() {
     const url = "http://coronavirus-19-api.herokuapp.com/countries";
@@ -8,9 +58,12 @@ function populateCountry() {
     .then( (res) => res.json())
     .then( countries => {
         for (const country of countries) {
-            countrySelect.innerHTML += `<option value = "">${country.country}</option>`
-            // countryName.innerHTML += `<h2>${country.country}</h2>`
-            // console.log(country);
+            countrySelect.innerHTML += `<option value = ${country.country}>${country.country}</option>`
+            
+        }
+        countrySelect.onchange=function(){
+            let value = countrySelect.value;
+            getCovidDashboard(value)
         }
         
         
@@ -19,4 +72,9 @@ function populateCountry() {
     
 }
 
-countrySelect.addEventListener('change', populateCountry());
+
+
+getCovidDashboard(start);
+populateCountry();
+
+
